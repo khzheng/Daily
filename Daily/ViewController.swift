d8608a91138610d4a0b31706105a1f76d8666471
Ken Zheng <kzheng@ucdavis.edu>
2018-07-11 00:01:08 -0700

    Add 'add' bar button item to display alert with textfield

--- Daily/ViewController.swift
+++ Daily/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.title = "Daily"
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.addButtonTapped(_:)))
+        self.navigationItem.rightBarButtonItem = addButton
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +25,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc func addButtonTapped(_ sender:UIBarButtonItem!) {
+        let ac = UIAlertController(title: nil, message: "What did you do today?", preferredStyle: .alert)
+        
+        let addAction = UIAlertAction(title: "Add", style: .default, handler: { (action: UIAlertAction) -> Void in
+            let dailyTextField = ac.textFields![0] as UITextField
+            
+            print(dailyTextField.text!)
+            })
+        addAction.isEnabled = false
+        ac.addAction(addAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        ac.addAction(cancelAction)
+        
+        ac.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Fixed a bug"
+            
+            NotificationCenter.default.addObserver(forName: Notification.Name.UITextFieldTextDidChange, object: textField, queue: OperationQueue.main, using: { (notification) in
+                addAction.isEnabled = textField.text != ""
+            })
+        })
+        
+        self.present(ac, animated: true, completion: nil)
+    }
 }
-
