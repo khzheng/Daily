185063c06a562415b49ef4c69a546ce2a59f56bc
Ken Zheng <kzheng@ucdavis.edu>
2018-07-10 22:27:40 -0700

    Initial commit. Removed main storyboard

--- Daily/AppDelegate.swift
+++ Daily/AppDelegate.swift
@@ -17,6 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let vc = ViewController()
+        let nc = UINavigationController(rootViewController: vc)
+        
+        self.window?.rootViewController = nc
+        self.window?.makeKeyAndVisible()
+        
+        print(#function)
+        
         return true
     }
 
